2dd31abbbc7f2ea3fd58b9d9b1af14768e36c61d
masaponto <masaponto@gmail.com>
2019-05-10 01:11:26 +0900

    [Add] component file for program of keisha maker

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <KeishaMaker/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import KeishaMaker from './components/KeishaMaker.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+      //HelloWorld
+	  KeishaMaker
   }
 }
 </script>
---
+++ src/components/KeishaMaker.vue
@@ -0,0 +1,60 @@
+<template>
+	<div>
+		<div class="Title">
+			<h1>Keisha Maker</h1>
+		</div>
+
+		<div class="bucho">
+			<a>bucho</a>
+			<input v-model="bucho_num" placeholder="0">
+			<input v-model="bucho_price" placeholder="0">
+		</div>
+
+		<div class="tanto-bucho">
+			<a>tanto-bucho</a>
+			<input v-model="tanto_bucho_num" placeholder="0">
+			<input v-model="tanto_bucho_price" placeholder="0">
+		</div>
+
+		<div class="kacho">
+			<a>kacho</a>
+			<input v-model="kacho_num" placeholder="0">
+			<input v-model="kacho_price" placeholder="0">
+		</div>
+
+		<div class="shusa">
+			<a>shusa</a>
+			<input v-model="shusa_num" placeholder="0">
+			<input v-model="shusa_price" placeholder="0">
+		</div>
+
+		<div class="hira">
+			<a>hira</a>
+			<input v-model="hira_num" placeholder="0">
+			<input v-model="hira_price" placeholder="0">
+		</div>
+
+	</div>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ h3 {
+	 margin: 40px 0 0;
+ }
+ ul {
+	 list-style-type: none;
+	 padding: 0;
+ }
+ li {
+	 display: inline-block;
+	 margin: 0 10px;
+ }
+ a {
+	 color: #42b983;
+ }
+</style>
